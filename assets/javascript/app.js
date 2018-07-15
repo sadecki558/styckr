534064feb7611c87eca2e10f6aeec8ffab370e8b
sadecki558 <matthewsadecki@gmail.com>
2018-07-15 12:13:57 -0400

    ajax somewhat working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,3 @@
-
 var map;
 var service;
 var marker;
@@ -10,21 +9,20 @@ var test = 0;
 var queryUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?";
 // "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=oakville&term=Starbucks"
 var result = [];
+var address = [];
+var queryUrl2 = "";
 //console.log(typs);
   
  
-function initialize() 
-{
+function initialize() {
     var mapOptions = {
-     zoom: 16,
-     mapTypeId: google.maps.MapTypeId.ROADMAP,
-     };
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    };
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
     //HTML5 geolocation
-    if (navigator.geolocation) 
-    {
-       navigator.geolocation.getCurrentPosition(function(position) 
-       {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
             pos = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
             infowindow = new google.maps.InfoWindow({map: map,position: pos,content: "You're Here!"});
            
@@ -35,13 +33,11 @@ function initialize()
             var service = new google.maps.places.PlacesService(map);
             service.nearbySearch(request,callback);
         }, 
-        function() 
-        { 
+        function() { 
         handleNoGeolocation(true);
         });
     } 
-    else 
-    {
+    else {
     handleNoGeolocation(false);
     }
  
@@ -67,23 +63,7 @@ function initialize()
         typs.push("bar");
         console.log("clicked" + "  " + clicker + "" + typs);
     }); 
-
-  //retrieves results
-  function callback(results, status) 
-    {
-      if (status == google.maps.places.PlacesServiceStatus.OK) 
-      {
-        for (var i = 0; i < results.length; i++) 
-        {    
-          createMarker(results[i]);
-          console.log(results[i]);
-          result.push(results[i].name);
-        }
-      }
-    }
-}
-//Makes a marker
-function createMarker(place, icon) {
+    function createMarker(place, icon) {
         var placeLoc = place.geometry.location;
         var marker = new google.maps.Marker({
             map: map,
@@ -91,15 +71,45 @@ function createMarker(place, icon) {
             icon: {
                 url: icon,
                 scaledSize: new google.maps.Size(10, 10) // pixels
-            },
-            animation: google.maps.Animation.DROP
-        });
-        
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.setContent(place.name+ '<br>' +place.vicinity);
-            infowindow.open(map, this);
-        });
+                },
+                animation: google.maps.Animation.DROP
+            });
+            
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.setContent(place.name+ '<br>' +place.vicinity);
+                infowindow.open(map, this);
+            });
+    }
+
+  //retrieves results
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {    
+                createMarker(results[i]);
+                console.log(results[i]);
+                result.push(results[i].name);
+                address.push(results[i].vicinity);
+            queryUrl2 = queryUrl + "location=" + address[5] + "&term="+result[5] + "&limit=1";
+            $.ajax({
+                method: "GET",
+                url: queryUrl2,
+                dataType: "json",
+                headers: {
+                    "Authorization": "Bearer gqqI1WuGp5Wr7QmZmrtJleBqhRGAVHibKExf_CtV2P7CFQ4LJgOI9gOX0zJ_-JdArDZXuvb-1mOFBsDfSoy7Rr9KJqJka3b837KqtJgQbROVBnOpbZSlgyEcKhVKW3Yx",
+                }}).then(function (response) {
+                    console.log(response);
+                    $(".card-title").empty();
+                    $(".card-title").append(response.businesses[0].name+ " rating: "+response.businesses[0].rating + " closed : "+ response.businesses[0].is_closed);
+                    $(".card-description").empty();
+                    $(".card-description").append("contact "+ response.businesses[0].name+ " at " + response.businesses[0].phone);
+            })
+            }
+        }
+    }
+    
 }
+//Makes a marker
+
         
 //zomato api
 /*$.ajax({
@@ -111,21 +121,5 @@ function createMarker(place, icon) {
     },
 })
 */
+//console.log(result);
 
-$.ajax({
-    method: "GET",
-    url: queryUrl + "location=" + result[0] + "&term="+result[1],
-    dataType: "json",
-    headers: {
-        "Authorization": "Bearer gqqI1WuGp5Wr7QmZmrtJleBqhRGAVHibKExf_CtV2P7CFQ4LJgOI9gOX0zJ_-JdArDZXuvb-1mOFBsDfSoy7Rr9KJqJka3b837KqtJgQbROVBnOpbZSlgyEcKhVKW3Yx",
-    }}).then(function (response) {
-        console.log(response);
-   /* //console.log(response);
-    for (i=0;i<response.businesses.length;i++){
-        //console.log(response.businesses[i].rating);
-    }
-    //console.log(result);
-    $(".card-title").append(response.businesses[0].name);
-    */
-
-})
